de2b8d1f528840c0709c5463e4734d8a4e0b9bc6
zidane <13521163@mahasiswa.itb.ac.id>
2022-11-09 05:44:12 +0700

    add js sentence support for cyk

--- cyk_algorithm.py
+++ cyk_algorithm.py
@@ -1,12 +1,56 @@
+def getReverse(char):
+    if char == ')': return '('
+    if char == '}': return '{'
+
+
+def getWords(str):
+    listOw = []
+    f_found = False
+    found_char = ''
+    c_string = ''
+
+    for char in str:
+        if (char == '(' or char == '{') and not f_found:
+            f_found = True
+            found_char = char
+
+        c_string += char
+
+        if (char == ')' or char == '}') and f_found and getReverse(char) == found_char:
+            f_found = False
+            listOw.append(c_string.strip())
+            c_string = ''
+            found_char = ''
+
+        if char == ' ' and not f_found and c_string.strip() != '':
+            listOw.append(c_string.strip())
+            c_string = ''
+
+    if c_string.strip() != '':
+        listOw.append(c_string.strip())
+
+    return listOw
+
+
 CNF = {
-    'S': {'A B', 'B B'},
+    'S': {'IF IF_N_R', 'B B'},
+    'IF_N_R': {'EXPR IF_N_R_2'},
+    'IF_N_R_2': {'STATEMENTS IF_N_R_3'},
+    'IF_N_R_3': {'ELSE_IF IF_N_R_4'},
+    'IF_N_R_4': {'EXPR STATEMENTS'},
+    'EXPR': {'(2+2 == 4)', '(3+c == 5)', '(3p+5+732)'},
+    'STATEMENTS': {'{foo.call()}'},
+    'ELSE_IF': {'elif'},
+    'IF': {'if', 'if'},
     'A': {'C C', 'A B', 'a'},
     'B': {'B B', 'C A', 'b'},
     'C': {'B A', 'A A', 'b'}
 }
 
-cnf_k_list = CNF.keys();
-string = "aaaabbba"
+cnf_k_list = CNF.keys()
+# "if (2+2 == 4) {foo.call()} elif (3+c == 5) {foo.call()}"
+list = getWords('if (3p+5+732) {foo.call()} elif (3+c == 5) {foo.call()}')
+string = 'aaaaa'
 str_n = len(string)
 arr_table = [['!' for j in range(str_n - i)] for i in range(str_n)]
 
@@ -17,10 +61,10 @@ for i in range(1, str_n + 1):
         cur_string = string[j:j + i]
         forms = []
 
+        # build possibilities of string
         tup = (cur_string[0:1],) if i == 1 else (cur_string[0:1], cur_string[1:])
         forms.append(tup)
 
-        # build possibilities of string
         for k in range(2, i):
             tup = (cur_string[0:k],) if k == i else (cur_string[0:k], cur_string[k:])
             forms.append(tup)
@@ -31,11 +75,11 @@ for i in range(1, str_n + 1):
 
             # check which variable could produce a word
             for el in cnf_k_list:
-                if c_char in CNF.get(el):
+                if c_char[0] in CNF.get(el):
                     str_pos.add(el)
 
             if len(str_pos) == 0:
-                arr_table[0][j] = '_'
+                arr_table[0][j] = str_pos
             else:
                 arr_table[0][j] = str_pos
 
@@ -49,19 +93,19 @@ for i in range(1, str_n + 1):
                 first_l = arr_table[len(form[0]) - 1][j]
                 # print(len(forms[0][1]) - 1, len(forms[0][0]) - 1, forms)
                 second_l = arr_table[len(form[1]) - 1][len(form[0]) + j]
-
                 # print(form, first_l, second_l)
 
                 for f_el in first_l:
                     for s_el in second_l:
                         c_str = f_el + ' ' + s_el
                         # print(c_str)
+                        # print(c_str)
                         for el in cnf_k_list:
                             if c_str in CNF.get(el):
                                 str_pos.add(el)
 
                 if len(str_pos) == 0:
-                    arr_table[r_idx][j] = '_'
+                    arr_table[r_idx][j] = str_pos
                 else:
                     arr_table[r_idx][j] = str_pos
 
@@ -71,7 +115,7 @@ print(
     '\n'.join('{}'.format(str(arr_table[k])[1:len(str(arr_table[k])) - 1]) for k in range(len(arr_table) - 1, -1, -1)))
 
 if 'S' in arr_table[len(string) - 1][0]:
-    print('string {} could be formed using CNF'.format(string))
+    print('string {} could be formed using CNF'.format(' '.join(string)))
 else:
     print('string {} could not be formed using CNF'.format(string))
 
