22adfff1227e35f16dc47fb46f49e543e7131c35
Marthenn <bintang.dwimarthen@gmail.com>
2022-11-25 02:30:20 +0700

    chore: test cases

---
+++ test/test1.js
@@ -0,0 +1,56 @@
+function foo(a,b,c){
+    for(var i=0;i<10;i++){
+        console.log(i);
+        if(i==5){
+            break;
+        } else {
+            console.log('else');
+        }
+    }
+    console.log(i);
+    return a+b+c;
+}
+
+const a = 1; console.log(a);
+switch(a){
+    case 1:
+        console.log('a is 1');
+        break;
+    case 2:
+        console.log('a is 2');
+        break;
+    default:
+        console.log('a is not 1 or 2');
+        break;
+}
+
+delete a;
+console.log(a);
+
+let j = 0;
+while(j<15){
+    j++;
+    if(j==5){continue}
+    console.log(j);
+}
+
+try{
+    console.log(j);
+}
+catch(e){
+    console.log(e);
+}
+finally{
+    console.log('finally');
+}
+
+while(true){
+    console.log('infinite loop');
+    break;
+}
+
+while(false){
+    console.log('no loop');
+}
+
+let as = null; console.log(as);
